014c054dda4c424b47d58ed733e74a4d2df11ceb
Alex2 <aleksander1802@gmail.com>
2023-09-10 14:09:24 +0300

    fix: fixed background images path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,10 +25,10 @@ const config: Config = {
 				accent: '#F13024',
 			},
 			backgroundImage: {
-				explosion: 'url("/bg-explosion.png")',
-				circles: 'url("/circles.png")',
-				circleStar: 'url("/circle-star.svg")',
-				site: 'url("/site-bg.svg")',
+				explosion: 'url("/cv/bg-explosion.png")',
+				circles: 'url("/cv/circles.png")',
+				circleStar: 'url("/cv/circle-star.svg")',
+				site: 'url("/cv/site-bg.svg")',
 			},
 			animation: {
 				'spin-slow': 'spin 6s linear infinite',
